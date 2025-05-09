ea62d3f84e590e3a894166b7be61dd9eda0aeecb
Hemchand Virat <crackerace04@gmail.com>
2025-05-09 20:07:37 +0530

    After mixed command again added & commited

--- digitalclock.py
+++ digitalclock.py
@@ -1,8 +1,24 @@
 
 from tkinter import Tk, Label
+from datetime import datetime
+
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
+#added features
+window.configure(bg="steelblue")
+label = Label(
+    window,
+    font = ("Arial Black", 78, "bold"),
+    bg   = "steelblue"
+)
+label.pack(pady=100)
+
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500,clock)
+clock()
 
 window.mainloop()
 
